5c3b802cafabbaab6240d41ae8d4457041b174cd
DiegoMutre <tysonmutre10@gmail.com>
2021-11-11 18:59:08 -0500

    * Create rollup config in txtasks ui

---
+++ packages/txtasks-ui/rollup.config.js
@@ -0,0 +1,40 @@
+import peerDeepExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+import packageJson from './package.json';
+
+// Extensions to be transpiled and resolved
+const extensions = ['.ts', '.tsx'];
+
+// Excluded dependencies from the package.json
+const external = Object.keys(packageJson.devDependencies);
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+    input: 'src/index.ts', // Bundle Entry file
+    output: {
+        dir: 'dist', // Folder where the bundle will be generated
+        sourcemap: true,
+        format: 'esm', // Format of the bundle, in this case `ESModules` (import/export)
+        preserveModules: true, // See https://rollupjs.org/guide/en/#outputpreservemodules
+    },
+    plugins: [
+        peerDeepExternal(), // Externalize `peerDependencies` from the bundle
+        resolve(), // Resolve `node_modules`
+        commonjs(), // Resolve commonjs modules
+        babel({
+            // Integration rollup with babel
+            extensions,
+            babelHelpers: 'inline',
+            include: extensions.map(ext => `src/**/*${ext}`),
+        }),
+    ],
+    external, // Dev dependencies to be excluded
+};
+
+// Is required to export default the config object
+export default config;
