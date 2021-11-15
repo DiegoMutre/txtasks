8a71b76ad994a65109c1c435579a0b02690e7d1b
DiegoMutre <tysonmutre10@gmail.com>
2021-11-15 06:16:05 -0500

    * Create jest config

---
+++ packages/txtasks-ui/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // The root of the source code
+    roots: ['<rootDir>/src'],
+
+    // this adds support for typescript
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+
+    // Run special logic, such as cleaning up components
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+
+    // Files pattern to test, must be inside a `__tests__` folder and must contain `.test` or `.spec` at the end of the filename
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+    // Module file extensions for importing
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
